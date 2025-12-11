7ec9ad51170b4df7f9ce36babca7e912f86cdb35
Lakshitha de Silva <6738265+LakshithadeSilva@users.noreply.github.com>
2025-12-11 15:47:17 +0000

    adds e2e tests

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 
@@ -14,13 +16,42 @@ const (
 	requestTimeout = 1 * time.Second
 )
 
-func TestHello(t *testing.T) {
-	request, err := http.NewRequest("GET", fmt.Sprintf("%v/hello", baseUrl), nil)
-	assert.NoError(t, err)
-
+func TestEndpoints(t *testing.T) {
 	client := &http.Client{Timeout: requestTimeout}
-	response, err := client.Do(request)
+	tests := []struct {
+		name   string
+		method string
+		url    string
+		body   io.Reader
+	}{
+		{
+			name:   "GetFileList",
+			method: http.MethodGet,
+			url:    fmt.Sprintf("%s/%s/files", baseUrl, "p0001"),
+		},
+		{
+			name:   "ApproveFile",
+			method: http.MethodPut,
+			url:    fmt.Sprintf("%s/%s/files/%s/approve", baseUrl, "p0001", "f1234"),
+			body:   strings.NewReader(`{"user_id":"user1"}`),
+		},
+		{
+			name:   "GetFile",
+			method: http.MethodGet,
+			url:    fmt.Sprintf("%s/%s/files/%s", baseUrl, "p0001", "f1234"),
+		},
+	}
 
-	assert.NoError(t, err)
-	assert.Equal(t, 200, response.StatusCode)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest(tc.method, tc.url, tc.body)
+			assert.NoError(t, err)
+			if tc.body != nil {
+				req.Header.Set("Content-Type", "application/json")
+			}
+			res, err := client.Do(req)
+			assert.NoError(t, err)
+			assert.Equal(t, http.StatusNotImplemented, res.StatusCode)
+		})
+	}
 }
