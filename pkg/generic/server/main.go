1b182923b35443568e64b8007d467733bb14b702
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2026-02-25 11:35:55 +0100

    Rename `server*.go` -> `main*.go`

--- pkg/generic/server/main.go
+++ pkg/generic/server/main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-//go:generate go tool oapi-codegen -generate gin -package server -o server.gen.go ../../../api/storage.yaml
+//go:generate go tool oapi-codegen -generate gin -package server -o main.gen.go ../../../api/storage.yaml
 //go:generate go tool oapi-codegen -generate spec -package server -o spec.gen.go ../../../api/storage.yaml
 //go:generate go tool oapi-codegen -generate types -package server -o types.gen.go ../../../api/storage.yaml
 
